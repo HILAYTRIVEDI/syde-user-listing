a9fc7eceeb43c1c52590986f2e78bc5f2dd57d3e
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-11-17 20:45:00 +0530

    Feat: add core plugin file

---
+++ syde-user-listing.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Syde User Listing
+ * Plugin URI: https://github.com/HILAYTRIVEDI/syde-user-listing
+ * Description: This Plugin will list the dummy data of users
+ * Version: 1.0.0
+ * Author: HILAYTRIVEDI
+ * Author URI: https://github.com/HILAYTRIVEDI
+ * License: GPL-3.0-or-later
+ * Text Domain: syde-user-listing
+ * 
+ * @package SydeUserListing
+ */
