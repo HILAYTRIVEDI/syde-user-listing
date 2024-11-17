7653458dc605edd934785d5e1128d472c2733a1e
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-11-17 22:15:04 +0530

    Feat: add all the controller, View and Services files

---
+++ src/Controllers/ShortCodeController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Syde\SydeUserListing\Controllers;
+
+/**
+ * Class ShourtCodeController
+ * 
+ * @package Syde\SydeUserListing\Controllers
+ */
+class ShortCodeController{}
---
+++ src/Services/APIService.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Syde\SydeUserListing\Services;
+
+/**
+ * Class ApiService
+ * 
+ * @package Syde\SydeUserListing\Services
+ */
+class APIService{}
