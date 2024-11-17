eabd6110747e7b20f956a756d7b66a28488ea67c
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-11-17 21:00:30 +0530

    Feat: add comments to the plugin class.

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace Syde\SydeUserListing;
 
+/**
+ * Class Plugin
+ * 
+ * @package Syde\SydeUserListing
+ */
 class Plugin{
     
+    /**
+     * Initialize the plugin.
+     * 
+     * @return void
+     * @since 1.0.0
+     * @access public
+     * @static
+     * 
+     */
     public static function init(): void
     {
         echo 'Hello World';
