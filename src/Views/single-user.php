fa43bdf6311a9cdecb155a0a23a180b890e17a39
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-11-20 00:59:20 +0530

    Feat add the styles and proper accessiblity to data views

--- src/Views/single-user.php
+++ src/Views/single-user.php
@@ -1,28 +1,14 @@
-<div class="table-responsive">
-    <table class="syde-user-listing-table" aria-describedby="user-table-desc">
-        <caption id="user-table-desc">
-            <?php echo esc_html__('Additional Data', 'syde-user-listing') ?>
-        </caption>
-        <thead>
-            <?php
-                $keys = array_keys($user_details);
-                foreach ($keys as $key) {
-                    echo '<th scope="col" data-label="' . esc_html($key) . '">' . esc_html(strtoupper($key)) . '</th>';
-                }
-
-            ?>
-        </thead>
-        <tbody>
-            <tr class="user-row" data-user-id="<?php echo esc_attr($value) ?>">
-                <?php
-                    array_walk_recursive($user_details, function ($value, $key) { ?>
-                        <td class="user-<?php esc_attr($key) ?>" data-label="<?php echo esc_attr($key) ?>">
-                            <?php echo esc_html($value) ?>
-                        </td>
-                    <?php
-                    });
-                ?>
-            </tr>
-        </tbody>
-    </table>
-</div>
+<ol class="syde-user-listing-table" aria-describedby="user-table-desc">
+    <caption id="user-table-desc">
+        <?php echo esc_html__('Additional Data', 'syde-user-listing') ?>
+    </caption>
+    <?php
+    array_walk_recursive($user_details, function ($value, $key) { ?>
+        <li class="user-<?php esc_attr($key) ?>" data-label="<?php echo esc_attr($key) ?>" data-value="<?php echo esc_attr($value) ?>">
+            <span><?php echo esc_attr(strtoupper($key)) ?>:</span>
+            <span><?php echo esc_attr($value) ?></span>
+        </li>
+    <?php
+    });
+    ?>
+</ol>
