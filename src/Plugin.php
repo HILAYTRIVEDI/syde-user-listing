1afb089bd6888a759f188bd79e6e36f0817feaf3
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-11-20 23:56:12 +0530

    Feat: add plugin class documents

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Main bootstrap file for the plugin.
  * 
@@ -19,30 +18,57 @@ use Syde\UserListing\Controllers\ShortcodeController;
 /**
  * Class Plugin
  * 
+ * The main plugin class that initializes and registers services.
+ * It is responsible for setting up the plugin and linking necessary controllers.
+ * 
  * @package Syde\UserListing
+ * @since 1.0.0
  */
-class Plugin{
+class Plugin
+{
 
-    public function __construct(private Container $container){}
-    
+    /**
+     * The container instance for dependency injection.
+     * 
+     * @param Container $container An instance of the container class.
+     * @return void
+     * @since 1.0.0
+     * @access public
+     */
+    public function __construct(private Container $container) {}
+
     /**
      * Initialize the plugin.
+     * 
+     * This method serves as the entry point for plugin initialization.
+     * It registers necessary services such as controllers for the admin panel
+     * and shortcodes.
      * 
      * @return void
      * @since 1.0.0
      * @access public
-     * @static
-     * 
      */
     public function init(): void
     {
+        // Register plugin services
         $this->registerServices();
     }
 
+    /**
+     * Register services such as controllers.
+     * 
+     * This method fetches the required controllers from the container and
+     * ensures that they are properly registered. It helps to decouple the plugin
+     * logic from WordPress by relying on the container for service management.
+     * 
+     * @return void
+     * @since 1.0.0
+     * @access public
+     */
     public function registerServices(): void
     {
-        $this->container->get(AdminController::class)->register();
-        $this->container->get(ShortcodeController::class);
-    }
+        $adminController = $this->container->get(AdminController::class)?->register();
 
+        $shortcodeController = $this->container->get(ShortcodeController::class);
+    }
 }
