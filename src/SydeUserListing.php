a99e160a1f8867e3eb5b301cede972d83bee4679
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-12-05 00:24:37 +0530

    Feat: remove redudency in the bootstrap class

--- src/SydeUserListing.php
+++ src/SydeUserListing.php
@@ -27,17 +27,6 @@ use Syde\UserListing\Controllers\AjaxController;
 final class SydeUserListing
 {
     /**
-     * Constructor.
-     *
-     * @param SydeContainer $container The dependency injection container.
-     * @since 1.0.0
-     */
-    public function __construct(private SydeContainer $container)
-    {
-        $this->container = $container;
-    }
-
-    /**
      * Bootstrap the plugin.
      *
      * This method initializes the plugin and registers its services.
@@ -47,24 +36,9 @@ final class SydeUserListing
      */
     public static function bootstrap(): void
     {
-        $plugin = new self(new SydeContainer());
-        $plugin->registerControllers();
-    }
-
-    /**
-     * Register necessary plugin services.
-     *
-     * This method ensures that controllers and other services are
-     * retrieved from the container and properly initialized.
-     *
-     * @return void
-     * @throws \RuntimeException If a required service is not available.
-     * @since 1.0.0
-     */
-    public function registerControllers(): void
-    {
-        $this->container->get(AdminController::class)?->register();
-        $this->container->get(ShortcodeController::class)->register();
-        $this->container->get(AjaxController::class)->register();
+        $plugin = new SydeContainer();
+        $plugin->get(AdminController::class)?->register();
+        $plugin->get(ShortcodeController::class)->register();
+        $plugin->get(AjaxController::class)->register();
     }
 }
