027810c653211d65acecd3024b6b9cb2f514dae3
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-12-06 16:23:16 +0530

    Revert "Feat: setup bootstrap login for brain monkey"
    This reverts commit 7bd970a1936a287c2f25d500dd0c1fde40518b09.

--- tests/SydeUserListingTest.php
+++ tests/SydeUserListingTest.php
@@ -5,19 +5,18 @@ declare(strict_types=1);
 namespace Syde\UserListing\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Brain\Monkey;
 
 class SydeUserListingTest extends TestCase
 {
-    protected function setUp(): void
+
+    public function setup(): void
     {
-        parent::setUp();
-        Monkey\setUp();
+        parent::setup();
     }
 
-    protected function tearDown(): void
+    public function tearDown(): void
     {
-        Monkey\tearDown();
         parent::tearDown();
     }
 }
+
